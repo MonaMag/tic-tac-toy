6b302f99cb5e419e8fec4bbe71c62171ef3c0eb0
Mona Mag <64916820+MonaMag@users.noreply.github.com>
2024-10-29 01:03:09 +0300

    changed handle

--- src/tic-tac-toy/TicTacToy.tsx
+++ src/tic-tac-toy/TicTacToy.tsx
@@ -3,6 +3,15 @@ import { Board } from './Board';
 
 export function TicTacToy() {
   const [size, setSize] = useState(3);
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+        const value = Number(e.currentTarget.value);
+        if (value >= 3 && value <= 10) {
+            setSize(value);
+        }
+    }
+};
   return (
     <div className="flex flex-col w-full items-center p-10 bg-gray-100">
       <h2 className='mb-5 text-2xl font-bold text-green-600 underline'>TicTacToy game</h2>
@@ -10,8 +19,8 @@ export function TicTacToy() {
         <input
           min="3"
           max="10"
-          value={size}
-          onChange={(e) => setSize(Number(e.target.value))}
+          defaultValue={size}
+          onKeyDown={handleKeyDown}
           className="p-2 border border-gray-300"
         />
       </div>
