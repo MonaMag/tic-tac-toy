09c3c145a9bc334d856d690cee56bf6a30afc8ef
Mona Mag <64916820+MonaMag@users.noreply.github.com>
2024-10-30 20:43:36 +0300

    fixed

--- src/snake/Snake.tsx
+++ src/snake/Snake.tsx
@@ -1,5 +1,9 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { twMerge } from 'tailwind-merge';
+
+const TIME_TO_MOVE = 200;
+
+type Position = { x: number; y: number };
 
 export function Snake({
   rows,
@@ -10,108 +14,94 @@ export function Snake({
   cols: number;
   snakeLength: number;
 }) {
-  const [snake, setSnake] = useState<{ x: number; y: number }[]>([]);
-  const [direction, setDirection] = useState<{ x: number; y: number }>({
-    x: 0,
-    y: -1,
-  });
-  const [isGameStart, setIsGameStart] = useState(false);
+  const [snake, setSnake] = useState<Position[]>([]);
   const [isAutoMove, setIsAutoMove] = useState(false);
   const [hitCount, setHitCount] = useState(0);
-  const [gameOver, setGameOver] = useState(false);
+
+  const isGameOver = hitCount >= 10;
+
+  const currentPositionRef = useRef<Position>({ x: 0, y: 0 });
+  const currentSnake = useRef<Position[]>(snake);
+  currentSnake.current = snake;
 
   useEffect(() => {
     const initialSnake = Array.from({ length: snakeLength }, (_, index) => ({
-      x: Math.floor(cols / 2) + Math.floor(snakeLength / 2) - index,
+      x: Math.floor(cols / 2) - Math.floor(snakeLength / 2) + index,
       y: Math.floor(rows / 2),
     }));
     setSnake(initialSnake);
   }, [snakeLength, cols, rows]);
 
-  const moveSnake = useCallback(() => {
-    if (!isGameStart || snake.length === 0) return;
+  const moveSnake = useCallback(
+    ({ x, y }: Position, currentSnake: Position[]) => {
+      const newSnake = [...currentSnake];
+      const headSnake = newSnake[0];
 
-    const newSnake = [...snake];
-    const head = newSnake[0];
+      const newHead = {
+        x: headSnake.x + x,
+        y: headSnake.y + y,
+      };
 
-    const newHead = {
-      x: head.x + direction.x,
-      y: head.y + direction.y,
-    };
-
-    // if (
-    //   newHead.x < 0 ||
-    //   newHead.x >= cols ||
-    //   newHead.y < 0 ||
-    //   newHead.y >= rows ||
-    //   snake.some((part) => part.x === newHead.x && part.y === newHead.y)
-    // ) {
-    //   setIsGameStart(false);
-    //   return;
-    // }
-
-    if (
-      newHead.x < 0 ||
-      newHead.x >= cols ||
-      newHead.y < 0 ||
-      newHead.y >= rows ||
-      snake.some((part) => part.x === newHead.x && part.y === newHead.y)
-    ) {
+      //сheck for out of bounds
       if (
         newHead.x < 0 ||
         newHead.x >= cols ||
         newHead.y < 0 ||
         newHead.y >= rows
       ) {
-        setHitCount((prevHits) => prevHits + 1);
+        setHitCount((prev) => prev + 1);
+
+        return currentSnake;
       }
 
-      if (hitCount >= 9) {
-        setGameOver(true);
-        setIsGameStart(false);
-        return;
+      //check for collision
+      if (
+        newSnake.some((part) => part.x === newHead.x && part.y === newHead.y)
+      ) {
+        return currentSnake;
       }
 
-      setIsGameStart(false);
-      return;
-    }
-    newSnake.unshift(newHead);
-    newSnake.pop();
-    setSnake(newSnake);
-  }, [cols, rows, snake, direction, isGameStart, hitCount]);
+      newSnake.unshift(newHead);
+      newSnake.pop();
+      setSnake(newSnake);
+      return newSnake;
+    },
+    [cols, rows]
+  );
 
   useEffect(() => {
-    if (isAutoMove && isGameStart) {
-      const interval = setInterval(moveSnake, 100);
+    if (isAutoMove) {
+      const interval = setInterval(
+        () => moveSnake(currentPositionRef.current, currentSnake.current),
+        TIME_TO_MOVE
+      );
       return () => clearInterval(interval);
     }
-  }, [isAutoMove, isGameStart, direction, snake, moveSnake]);
+  }, [isAutoMove, moveSnake]);
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
+      let currentPosition: Position | null = null;
       switch (event.key) {
         case 'ArrowLeft':
-          setDirection({ x: -1, y: 0 });
+          currentPosition = { x: -1, y: 0 };
           break;
         case 'ArrowRight':
-          setDirection({ x: 1, y: 0 });
+          currentPosition = { x: 1, y: 0 };
           break;
         case 'ArrowUp':
-          setDirection({ x: 0, y: -1 });
+          currentPosition = { x: 0, y: -1 };
           break;
         case 'ArrowDown':
-          setDirection({ x: 0, y: 1 });
+          currentPosition = { x: 0, y: 1 };
           break;
       }
-      if (!isAutoMove) {
-        moveSnake();
-      }
-
-      if (!isGameStart) {
-        setIsGameStart(true);
-        moveSnake();
-      } else {
-        moveSnake();
+      if (currentPosition) {
+        currentPositionRef.current = currentPosition;
+        if (!isAutoMove) {
+          const newSnake = moveSnake(currentPosition, currentSnake.current);
+          setSnake(newSnake);
+        }
       }
     };
 
@@ -119,11 +109,9 @@ export function Snake({
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
     };
-  }, [isGameStart, direction, isAutoMove, moveSnake]);
+  }, [isAutoMove, moveSnake]);
 
-  const toggleMode = () => {
-    setIsAutoMove(!isAutoMove);
-  };
+  console.log('hitCount', hitCount);
 
   return (
     <div className="flex flex-col w-full items-center p-10">
@@ -131,8 +119,8 @@ export function Snake({
         Snake game
       </h2>
       <button
-        onClick={toggleMode}
-        className="mb-4 px-6 py-1 border border-blue-400  text- rounded"
+        onClick={() => setIsAutoMove((x) => !x)}
+        className="mb-4 px-6 py-1 border border-blue-400 rounded"
       >
         {isAutoMove ? 'click' : 'auto'}
       </button>
@@ -164,7 +152,7 @@ export function Snake({
           })}
         </div>
       ))}
-      {gameOver && (
+      {isGameOver && (
         <div className="mt-4 px-6  text-red-400 text-lg">
           Game over! You hit the border 10 times.
         </div>
